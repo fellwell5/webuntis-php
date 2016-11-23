059bc5a86d3e9909b47993efef5079839c7c0e25
Matthias Schaffer <matthias@poechinger.com>
2016-11-23 13:02:49 +0100

    Create example.php

---
+++ example.php
@@ -0,0 +1,19 @@
+<?php
+
+
+    require "webuntis.php";
+
+    $untis = new Webuntis();
+    
+    #auth('url', 'schoolname', 'username', 'password');
+    $untis->auth('arche.webuntis.com', 'litec', '40146720140010', '_passwort_');
+    
+    $table      = $untis->getTimetable();
+    $subjects   = $untis->getSubjects();
+    $teachers   = $untis->getTeachers();
+    $rooms      = $untis->getRooms();
+
+    print_r($table);
+    print_r($subjects);
+    $untis->logout();
+?>
